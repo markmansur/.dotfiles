a9db5f0ecde2488b9e5f7b9a6e7d80a802670c04
Mark <mmansur2@uwo.ca>
2022-04-09 12:19:54 -0400

    initial commit

---
+++ zsh/alias.zsh
@@ -0,0 +1,12 @@
+
+# terminal
+alias c="clear"
+
+
+# git
+alias gst="git status"
+
+#python
+alias python="python3"
+alias pip="pip3"
+
---
+++ zsh/setup.sh
@@ -0,0 +1,3 @@
+#!/bin/zsh
+
+ln -s dotfiles/zsh/zshrc ~/.zshrc
