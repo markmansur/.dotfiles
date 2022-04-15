efdf1c10e1940320f3d703be8af85ace4a14a541
Mark <mmansur2@uwo.ca>
2022-04-15 15:58:45 -0400

    add keymaps.lua

---
+++ vim/lua/user/keymaps.lua
@@ -0,0 +1,20 @@
+local opts = { noremap = true, silent = true }
+
+-- shorten function name
+local keymap = vim.api.nvim_set_keymap
+
+-- Modes
+--  normal_mode = "n"
+--  insert_mode = "i"
+--  visual_mode = "v"
+--  visual_block_mode = "x"
+--  term_mode = "t"
+--  command_mode = "c"
+
+-- Normal
+-- Better window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
