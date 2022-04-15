d67fa9bcb84363c1eedc63f2b1dffe5029a83630
Mark <mmansur2@uwo.ca>
2022-04-15 13:55:57 -0400

    add more options

--- vim/lua/user/options.lua
+++ vim/lua/user/options.lua
@@ -1,8 +1,12 @@
 local options = {
-  number = true,
-  relativenumber = true
+	number = true,
+	relativenumber = true,
+	tabstop = 4,
+	softtabstop = 4,
+	shiftwidth = 4,
+	scrolloff = 8
 }
 
 for k, v in pairs(options) do
-  vim.opt[k] = v
+	vim.opt[k] = v
 end
