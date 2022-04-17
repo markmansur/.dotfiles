c39554636fbd7be70a0272a65c748d1f5ca44aec
Mark <mmansur2@uwo.ca>
2022-04-17 11:56:45 -0400

    safely set color scheme

--- vim/lua/user/colorscheme.lua
+++ vim/lua/user/colorscheme.lua
@@ -1,1 +1,7 @@
-vim.cmd("colorscheme gruvbox")
+local colorscheme = "gruvbox"
+
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+  vim.notify("colorscheme " .. colorscheme .. " not found!")
+  return
+end
