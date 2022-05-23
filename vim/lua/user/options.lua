81f9b626fb4d47834ce1c5a0bb801ca917bf307b
Mark <mmansur2@uwo.ca>
2022-05-23 09:40:19 -0400

    Use 2 spaces for tabs

--- vim/lua/user/options.lua
+++ vim/lua/user/options.lua
@@ -1,9 +1,9 @@
 local options = {
     number = true,
     relativenumber = true,
-    tabstop = 4,
-    softtabstop = 4,
-    shiftwidth = 4,
+    tabstop = 2,
+    softtabstop = 2,
+    shiftwidth = 2,
     expandtab = true,
     scrolloff = 8,
     termguicolors = true,
