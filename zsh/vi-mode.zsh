ecb13397884e6e7392c2010a39a275132e7899f3
Mark <mmansur2@uwo.ca>
2022-09-18 11:43:22 -0400

    Add vi mode for zsh

---
+++ zsh/vi-mode.zsh
@@ -0,0 +1,6 @@
+# Plugin for vi mode in zsh
+# https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/vi-mode
+# Blog post: https://www.barbarianmeetscoding.com/blog/5-minutes-vim-enabling-vim-in-zsh
+MODE_INDICATOR="%F{white}n%f"
+INSERT_MODE_INDICATOR="%F{yellow}i%f"
+VI_MODE_RESET_PROMPT_ON_MODE_CHANGE=true
