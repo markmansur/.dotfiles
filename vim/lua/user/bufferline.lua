c59ce8ee94d2c37ea8c1d39d30aeb9ab5e6badaf
Mark <mmansur2@uwo.ca>
2022-09-27 22:01:41 -0400

    Add LSP info to bufferline

--- vim/lua/user/bufferline.lua
+++ vim/lua/user/bufferline.lua
@@ -2,6 +2,16 @@ require("bufferline").setup{
   options = {
     show_buffer_close_icons = false,
     sort_by= 'insert_at_end',
+    diagnostics = "nvim_lsp",
+    diagnostics_indicator = function(count, level, diagnostics_dict, context)
+      local s = " "
+      for e, n in pairs(diagnostics_dict) do
+        local sym = e == "error" and " "
+          or (e == "warning" and " " or " " )
+        s = s .. n .. sym
+      end
+      return s
+    end,
     offsets = {
       {
         filetype = "NvimTree",
