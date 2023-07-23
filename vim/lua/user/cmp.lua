fc8cda7f416b740d3e7e713e928678111f7562d6
Mark <mmansur2@uwo.ca>
2023-07-22 23:14:07 -0400

    Add html snippets to tsx

--- vim/lua/user/cmp.lua
+++ vim/lua/user/cmp.lua
@@ -9,6 +9,8 @@ if not snip_status_ok then
   return
 end
 
+luasnip.filetype_extend("typescriptreact", { "html" })
+
 require("luasnip/loaders/from_vscode").lazy_load()
 
 local check_backspace = function()
