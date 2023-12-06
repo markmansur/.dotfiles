2ada7b08bfec81c9344bbcc63f549cfee32e26f1
Mark <mmansur2@uwo.ca>
2023-12-05 21:14:03 -0500

    Remove encoding from lualine

--- vim/lua/user/lualine.lua
+++ vim/lua/user/lualine.lua
@@ -21,7 +21,6 @@ lualine.setup {
         lualine_x = {
             { 'diagnostics', sources = { "nvim_diagnostic" }, symbols = { error = ' ', warn = ' ', info = ' ',
               hint = ' ' } },
-            'encoding',
             'filetype'
           },
         -- lualine_y = { 'progress' },
