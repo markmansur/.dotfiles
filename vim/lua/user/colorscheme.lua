40b8334bffac4a2664276458f1c69e047f1cab31
Mark <mmansur2@uwo.ca>
2024-08-10 17:54:51 -0400

    add tokyonight colorscheme

--- vim/lua/user/colorscheme.lua
+++ vim/lua/user/colorscheme.lua
@@ -1,25 +1,42 @@
-local status_ok, gruvbox= pcall(require, "gruvbox")
-if not status_ok then
-	return
+local theme = 'tokyonight'
+
+if theme == 'gruvbox' then
+  local status_ok, gruvbox= pcall(require, "gruvbox")
+  if not status_ok then
+	  return
+  end
+  
+  contrast = "soft" -- "hard" | "soft" | ""
+
+  gruvbox.setup({
+    terminal_colors = true,
+    contrast = contrast,
+    transparent_mode = true,
+    overrides = {
+      NvimTreeFolderIcon = {
+        link = 'GruvBoxOrange'
+      },
+      NvimTreeEndOfBuffer = {
+        link = 'GruvBoxBg0'
+      },
+      SignColumn = {
+        link = 'GruvBoxBg0'
+      }
+    }
+  })
+
+  vim.cmd('colorscheme gruvbox')
 end
 
-contrast = "soft" -- "hard" | "soft" | ""
+if theme == 'tokyonight' then
+  local status_ok, tokyonight = pcall(require, "tokyonight")
+  if not status_ok then
+	  return
+  end
 
-gruvbox.setup({
-  terminal_colors = true,
-  contrast = contrast,
-  transparent_mode = true,
-  overrides = {
-    NvimTreeFolderIcon = {
-      link = 'GruvBoxOrange'
-    },
-    NvimTreeEndOfBuffer = {
-      link = 'GruvBoxBg0'
-    },
-    SignColumn = {
-      link = 'GruvBoxBg0'
-    }
-  }
-})
+  tokyonight.setup({})
 
-vim.cmd('colorscheme gruvbox')
+  vim.cmd('colorscheme tokyonight')
+end
+
+
