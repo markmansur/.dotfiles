f4103352ec09d4f6f8be424d03e4286f100f13ad
Mark <mmansur2@uwo.ca>
2024-08-10 21:41:22 -0400

    remove theme from lualine

--- vim/lua/user/lualine.lua
+++ vim/lua/user/lualine.lua
@@ -5,7 +5,6 @@ if (not status) then return end
 lualine.setup {
     options = {
         icons_enabled = true,
-        theme = 'gruvbox',
         section_separators = { left = '', right = '' },
         component_separators = { left = '', right = '' },
         disabled_filetypes = {}
